43f33b637b95307004ddace2451c71ce2331acb9
Cristian Bernardes <cristian.aob@gmail.com>
2024-09-29 16:21:50 -0300

    Update ComprasViewService.php

--- projects/bibliotecaVirtual/app/Services/ComprasViewService.php
+++ projects/bibliotecaVirtual/app/Services/ComprasViewService.php
@@ -4,15 +4,28 @@ namespace App\Services;
 
 use App\Models\ComprasView;
 
+/**
+ *
+ */
 class ComprasViewService
 {
+    /**
+     * @var ComprasView
+     */
     private ComprasView $model;
 
+    /**
+     *
+     */
     public function __construct()
     {
         $this->model = new ComprasView();
     }
 
+    /**
+     * @param int|null $idUsuario
+     * @return mixed
+     */
     public function getComprasView(?int $idUsuario = null)
     {
         return $this->model->when($idUsuario, function ($query) use ($idUsuario) {

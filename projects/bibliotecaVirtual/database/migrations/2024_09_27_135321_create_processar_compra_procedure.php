f7d155f3ac2061f288ea6aaf42669029fbbdba04
Cristian Bernardes <cristian.aob@gmail.com>
2024-09-30 09:34:20 -0300

    Update 2024_09_27_135321_create_processar_compra_procedure.php

--- projects/bibliotecaVirtual/database/migrations/2024_09_27_135321_create_processar_compra_procedure.php
+++ projects/bibliotecaVirtual/database/migrations/2024_09_27_135321_create_processar_compra_procedure.php
@@ -28,23 +28,12 @@ return new class extends Migration {
                 -- Obtém o comprimento do JSON (quantidade de livros)
                 SET json_length = JSON_LENGTH(p_livros);
 
-                -- Cria a compra inicialmente sem calcular o valor
-                INSERT INTO compras(user_id, valor_compra, desconto, transacao_sucesso, created_at, updated_at)
-                VALUES (p_user_id, 0, p_desconto, 1, NOW(), NOW());
-
-                -- Obtém o ID da compra recém inserida
-                SET p_compra_id = LAST_INSERT_ID();
-
-                -- Loop para inserir os livros associados e calcular o valor total
+                -- Loop para calcular o valor total e verificar os livros
                 WHILE i < json_length DO
                     SET v_livro_id = JSON_UNQUOTE(JSON_EXTRACT(p_livros, CONCAT('$[', i, '].livro_id')));
                     SET v_quantidade = JSON_UNQUOTE(JSON_EXTRACT(p_livros, CONCAT('$[', i, '].quantidade')));
 
-                    -- Inserir na tabela compra_livros
-                    INSERT INTO compra_livros (compra_codcompra, livro_codl, quantidade, created_at, updated_at)
-                    VALUES (p_compra_id, v_livro_id, v_quantidade, NOW(), NOW());
-
-                    -- Atualizar o valor total da compra
+                    -- Calcular o valor total da compra
                     SET v_total = v_total + (SELECT valor * v_quantidade FROM livro WHERE codl = v_livro_id);
 
                     SET i = i + 1;
@@ -53,10 +42,25 @@ return new class extends Migration {
                 -- Aplicar o desconto ao valor total
                 SET v_total = v_total - (v_total * p_desconto / 100);
 
-                -- Atualizar o valor total da compra na tabela compras
-                UPDATE compras
-                SET valor_compra = v_total
-                WHERE codcompra = p_compra_id;
+                -- Criar a compra com o valor já calculado
+                INSERT INTO compras(user_id, valor_compra, desconto, transacao_sucesso, created_at, updated_at)
+                VALUES (p_user_id, v_total, p_desconto, 1, NOW(), NOW());
+
+                -- Obtém o ID da compra recém inserida
+                SET p_compra_id = LAST_INSERT_ID();
+
+                -- Inserir os livros associados
+                SET i = 0;
+                WHILE i < json_length DO
+                    SET v_livro_id = JSON_UNQUOTE(JSON_EXTRACT(p_livros, CONCAT('$[', i, '].livro_id')));
+                    SET v_quantidade = JSON_UNQUOTE(JSON_EXTRACT(p_livros, CONCAT('$[', i, '].quantidade')));
+
+                    -- Inserir na tabela compra_livros
+                    INSERT INTO compra_livros (compra_codcompra, livro_codl, quantidade, created_at, updated_at)
+                    VALUES (p_compra_id, v_livro_id, v_quantidade, NOW(), NOW());
+
+                    SET i = i + 1;
+                END WHILE;
             END
         ");
     }
